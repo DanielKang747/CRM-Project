0a20be60be048e9c2444db43c86abb897d84efec
DanielKang747 <danielkang747@gmail.com>
2025-08-26 16:39:43 -0700

    workspace_ver0007_revised_vincent

---
+++ workspace_ver0007_revised_vincent/CustomerCare/src/DatabaseConstant.java
@@ -0,0 +1,9 @@
+
+public class DatabaseConstant
+{
+	public final static String DRIVERNAME = "com.mysql.jdbc.Driver";
+	public final static String URL = "jdbc:mysql://localhost:3306/customer_finder";
+	public final static String USER = "root";
+	public final static String PASSWORD = "qwe123";
+
+}
